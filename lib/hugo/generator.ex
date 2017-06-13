bc9ff00d90f60cd19da423f679eef4053391d14e
PABLO MARTINEZ ALVAREZ <pablo.martinez.alvarez@gmail.com>
2017-06-13 13:43:25 +0200

    hugo category folders

--- lib/hugo/generator.ex
+++ lib/hugo/generator.ex
@@ -6,29 +6,31 @@ defmodule LoremImpsumMd.Hugo.Generator do
   def generate(args) do
     File.mkdir_p(posts_folder())
     LoremImpsumMd.Categories.tree(args[:categories], args[:children], args[:levels])
-    |> populate_site(args, [])
+    |> populate_site(posts_folder(), args, [])
   end
 
   defp posts_folder do
     Path.join(@content_folder, @posts_folder)
   end
 
-  defp populate_site([], args, category_tokens) do
-    generate_articles(args, category_tokens)
+  defp populate_site([], root, args, category_tokens) do
+    File.mkdir(root)
+    generate_articles(root, args, category_tokens)
   end
 
-  defp populate_site(categories, args, category_tokens) do
+  defp populate_site(categories, root, args, category_tokens) do
+    File.mkdir(root)
     Enum.each(categories, fn {category, subcategories} ->
-      populate_site(subcategories, args, category_tokens ++ [category])
+      populate_site(subcategories, Path.join(root, category), args, category_tokens ++ [category])
     end)
   end
 
-  defp generate_articles(args, category_tokens) do
+  defp generate_articles(root, args, category_tokens) do
     num_articles = div(args[:size], args[:categories] * args[:children] * max(args[:levels] - 1, 1))
     Enum.map(1..num_articles, fn n ->
       Task.async(fn ->
         title = List.last(category_tokens) <> "_" <> "art_" <> to_string(n)
-        File.write(Path.join(posts_folder(), title <> ".md"), LoremImpsumMd.Hugo.ArticleTemplate.tpl(title, category_tokens, []))
+        File.write(Path.join(root, title <> ".md"), LoremImpsumMd.Hugo.ArticleTemplate.tpl(title, category_tokens, []))
       end)
     end)
     |> Enum.map(&Task.await/1)
